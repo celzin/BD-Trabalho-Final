da29ce01165f06ed1bde63a58c3500b77764908f
celsin <celsovinicius4@gmail.com>
2024-09-02 23:08:04 -0300

    retirando comentários

--- src/main.py
+++ src/main.py
@@ -41,41 +41,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# import os
-# from fpdf import FPDF
-
-# #------------------------------------------------------------------EXECUÇÃO-------------------------------------------------------------------------------------------------------------------------------
-# while True:
-#     print()
-#     print("--------------------------------MENU----------------------------------------")
-#     print()
-#     print("1 - Cadastrar doença.")
-#     print("2 - Listar doenças.")
-#     print("3 - Pesquisar doenças.")
-#     print("4 - Diagnóstico.")
-#     print("5 - Relatório.")
-#     print("6 - Logs.")
-#     print("7 - Sair.")
-#     print()
-#     opcao = int(input("Escolha uma opção: "))
-#     if opcao == 1:
-#         cadastrar_doenca()
-#     elif opcao == 2:
-#         listar_doenca()
-#     elif opcao == 3:
-#         pesquisar_doenca()
-#     elif opcao == 4:
-#         diagnostico()
-#     elif opcao == 5:
-#         relatorios()
-#     elif opcao == 6:
-#         print('Em Construção!')
-#     elif opcao == 7:
-#         print("7 - Sair.")
-#         break
-#     else:
-#         print("Opção inválida. Escolha um valor entre 1 e 7.")
-    
-    
     
